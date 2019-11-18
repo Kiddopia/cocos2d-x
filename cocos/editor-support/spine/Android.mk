5e1ae92751bde8d440fb3c08d477f2912efff8d2
Kaushik <kaushik@paperboatapps.com>
2019-11-18 16:13:54 +0530

    updated spine

--- cocos/editor-support/spine/Android.mk
+++ cocos/editor-support/spine/Android.mk
@@ -1,4 +1,4 @@
-
+	
 
 
 LOCAL_PATH := $(call my-dir)
@@ -10,57 +10,17 @@ LOCAL_MODULE_FILENAME := libspine
 
 LOCAL_ARM_MODE := arm
 
-LOCAL_SRC_FILES := \
-Animation.c \
-AnimationState.c \
-AnimationStateData.c \
-Array.c \
-Atlas.c \
-AtlasAttachmentLoader.c \
-Attachment.c \
-AttachmentLoader.c \
-AttachmentVertices.cpp \
-Bone.c \
-BoneData.c \
-BoundingBoxAttachment.c \
-ClippingAttachment.c \
-Cocos2dAttachmentLoader.cpp \
-Color.c \
-Event.c \
-EventData.c \
-IkConstraint.c \
-IkConstraintData.c \
-Json.c \
-MeshAttachment.c \
-PathAttachment.c \
-PathConstraint.c \
-PathConstraintData.c \
-PointAttachment.c \
-RegionAttachment.c \
-Skeleton.c \
-SkeletonAnimation.cpp \
-SkeletonBatch.cpp \
-SkeletonBinary.c \
-SkeletonBounds.c \
-SkeletonClipping.c \
-SkeletonData.c \
-SkeletonJson.c \
-SkeletonRenderer.cpp \
-SkeletonTwoColorBatch.cpp \
-Skin.c \
-Slot.c \
-SlotData.c \
-TransformConstraint.c \
-TransformConstraintData.c \
-Triangulator.c \
-VertexAttachment.c \
-VertexEffect.c \
-extension.c \
-spine-cocos2dx.cpp
+
+C_FILES := $(shell find $(LOCAL_PATH)/src/spine -name *.c)
+CPP_FILES := $(shell find $(LOCAL_PATH)/src/spine -name *.cpp)
+
+LOCAL_SRC_FILES := $(CPP_FILES:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(C_FILES:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
 
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/..
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/..
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/include
 
 LOCAL_STATIC_LIBRARIES := cc_core
 
