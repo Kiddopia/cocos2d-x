07c4ccf8358eeb038a18a6c66d19752950710911
Kaushik Poria <kaushik@paperboatapps.com>
2020-08-03 13:46:30 +0530

    [New] added spine in pba-external and updated cmake to create spine library

---
+++ pba-external/spine/Android.mk
@@ -0,0 +1,27 @@
+	
+
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := spine
+
+LOCAL_MODULE_FILENAME := libspine
+
+LOCAL_ARM_MODE := arm
+
+
+C_FILES := $(shell find $(LOCAL_PATH)/src/spine -name *.c)
+CPP_FILES := $(shell find $(LOCAL_PATH)/src/spine -name *.cpp)
+
+LOCAL_SRC_FILES := $(CPP_FILES:$(LOCAL_PATH)/%=%)
+LOCAL_SRC_FILES += $(C_FILES:$(LOCAL_PATH)/%=%)
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/..
+LOCAL_EXPORT_C_INCLUDES += $(LOCAL_PATH)/include
+
+LOCAL_STATIC_LIBRARIES := cc_core
+
+include $(BUILD_STATIC_LIBRARY)
